d7b02cc0a5079ccb46d4579f76246776e9e4c503
Endi Merkuri <merkuriendi@gmail.com>
2020-12-04 14:05:08 +0100

    Add awesomewm configs

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+run nm-applet &&
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &&
+# run compton
