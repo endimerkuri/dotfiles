da9396a787464cc62a72a1bff79076458c04f085
Endi Merkuri <merkuriendi@gmail.com>
2021-01-10 01:51:29 +0100

    Change emoji file location

--- software/emojiDmenu.sh
+++ software/emojiDmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /home/endi/emoji | dmenu -l 20 | cut -d' ' -f 1 | xclip -r -selection clipboard
+cat ~/.local/share/emoji | dmenu -l 20 | cut -d' ' -f 1 | xclip -r -selection clipboard
