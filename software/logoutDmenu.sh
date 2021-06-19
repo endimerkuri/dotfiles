5b96468045970336f0d063c50bbb1558ada99d79
Endi Merkuri <merkuriendi@gmail.com>
2021-06-19 23:58:06 +0200

    Logout menu for dwm

--- software/logoutDmenu.sh
+++ software/logoutDmenu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-res=$(echo "lock\nlogout\nshutdown\nreboot\ncancel" | rofi -dmenu -l 5 -p "Logout")
+res=$(echo "lock\nlogout\nshutdown\nreboot\ncancel" | dmenu -l 5 -p "Logout")
 
 case $res in
     lock)
         i3lock -c 000000
         ;;
     logout)
-        # dwmc quit
-        openbox --exit
+        dwmc quit
+        # openbox --exit
         ;;
     shutdown)
         systemctl poweroff 
