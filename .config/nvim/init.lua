7cfa87774d045a67372ea607d5ec625bfa732e64
Endi Merkuri <35932875+endimerkuri@users.noreply.github.com>
2021-09-03 11:42:54 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,6 +200,9 @@ sources = {
 -- " nmap <leader>drc <Plug>VimspectorRunToCursor
 -- " nmap <leader>dbp <Plug>VimspectorToggleBreakpoint
 -- " nmap <leader>dcbp <Plug>VimspectorToggleConditionalBreakpoint
+vim.api.nvim_set_keymap('n', '<leader>dc', ':Dotenv config/.env.development<CR>:lua require\'dap\'.continue()<CR>', { noremap = true } )
+vim.api.nvim_set_keymap('n', '<leader>db', ':lua require\'dap\'.toggle_breakpoint()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>dl', ':lua require\'dap\'.step_into()<CR>', { noremap = true })
 
 -- DBUI
 vim.api.nvim_set_keymap('n', '<leader>od', ':DBUIToggle<CR>', { noremap = true })
@@ -292,3 +295,22 @@ require'nvim-treesitter.configs'.setup {
         enable = true,              -- false will disable the whole extension
     },
 }
+local dap = require'dap'
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+dap.configurations.javascript = {
+  {
+    type = 'node2',
+    request = 'launch',
+    program = '${workspaceFolder}/index.js',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+}
+require'dapui'.setup()
+
