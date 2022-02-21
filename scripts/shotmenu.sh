d26dba8dcda8a85357bd26915f9ad2a406835c32
Endi Merkuri <merkuriendi@gmail.com>
2022-02-21 12:29:23 +0300

    Non transparent border picom

--- scripts/shotmenu.sh
+++ scripts/shotmenu.sh
@@ -3,17 +3,21 @@
 res=$(echo "full\nregion\nwindow\ncancel" | dmenu -p "Screenshot")
 filename=~/Pictures/screenshot-$(date +%F-%M-%S).png
 command="maim -d 1 -u $filename"
+sendNot="notify-send -t 1000 \"Screenshot!!!\""
 
 case $res in
     full)
         $command
+        $sendNot
         ;;
     region)
         $command -s
+        $sendNot
         ;;
     window)
         id=$(xdotool selectwindow)
         $command -i $id
+        $sendNot
         ;;
     *)
         ;;
