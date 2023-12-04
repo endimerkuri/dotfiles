f775e71cd1d67c63abbcf7908bf8f852ac86bcf0
Endi Merkuri <merkuriendi@gmail.com>
2023-12-04 10:26:24 +0100

    nvim trouble

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -99,5 +99,19 @@ return {
                 end
             }
         end
+    },
+    {
+        'folke/trouble.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opt = {
+
+        },
+        config = function ()
+            vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+            vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+            vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+            vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+            vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+        end
     }
 }
