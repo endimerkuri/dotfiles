5c73e685e329a8686e8a5dc55452adc86d0de2f1
Endi Merkuri <merkuriendi@gmail.com>
2024-12-12 17:20:58 +0100

    Update dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -23,6 +23,7 @@ return {
                     { icon = " ", key = "p", desc = "Projects", action = ":lua require'telescope'.extensions.project.project{ display_type = 'full' }" },
                     { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
                     { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "d", desc = "Database", action = ":ene | DBUIToggle" },
                     { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                     { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
                     { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
@@ -76,7 +77,7 @@ return {
         notifier = { enabled = false },
         quickfile = { enabled = false },
         scroll = { enabled = false },
-        statuscolumn = { enabled = false },
+        statuscolumn = { enabled = true },
         words = { enabled = false },
     },
 }
