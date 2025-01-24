0111bfc2805074e9997436a4b2fdc0c47aef18b5
Endi Merkuri <merkuriendi@gmail.com>
2025-01-24 11:59:25 +0100

    emacs: fix packages loading order

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,6 +1,6 @@
 (load-file (expand-file-name "better-defaults.el" user-emacs-directory))
-(load-file (expand-file-name "nerd-icons-config.el" user-emacs-directory))
 (load-file (expand-file-name "packages.el" user-emacs-directory))
+(load-file (expand-file-name "nerd-icons-config.el" user-emacs-directory))
 (load-file (expand-file-name "ui.el" user-emacs-directory))
 
 (setq gc-cons-threshold (or bedrock--initial-gc-threshold 800000))
