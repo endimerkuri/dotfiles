7473486bb5c89c9e754d2bcfc2181611ea382075
Endi Merkuri <merkuriendi@gmail.com>
2025-01-27 16:45:58 +0100

    emacs: mode line config in separate file

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2,33 +2,6 @@
 (load-file (expand-file-name "packages.el" user-emacs-directory))
 (load-file (expand-file-name "nerd-icons-config.el" user-emacs-directory))
 (load-file (expand-file-name "ui.el" user-emacs-directory))
-
-(defun my-modeline--major-mode-name ()
-  "Return capitalized `major-mode' as a string."
-  (format "%18s" (capitalize (replace-regexp-in-string "-mode" "" (symbol-name major-mode)))))
-
-(defvar-local my-modeline-major-mode
-    '(:eval
-      (propertize (my-modeline--major-mode-name) 'face 'bold))
-  "Mode line construct to display the major mode.")
-
-(put 'my-modeline-major-mode 'risky-local-variable t)
-
-(defun mode-line-fill (reserve)
-  "Return empty space using FACE and leaving RESERVE space on the right."
-  (when
-      (and window-system (eq 'right (get-scroll-bar-mode)))
-    (setq reserve (- reserve 3)))
-  (propertize " "
-              'display
-              `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))))
-
-(setq-default mode-line-format
-              (list '("%e" mode-line-front-space
-                      (:propertize "[%*] " display (min-width ...))
-                      mode-line-buffer-identification "   " mode-line-position
-                      (vc-mode vc-mode))
-                    "  "  mode-line-misc-info "  "
-                    (mode-line-fill 20) my-modeline-major-mode))
+(load-file (expand-file-name "modeline.el" user-emacs-directory))
 
 (setq gc-cons-threshold (or bedrock--initial-gc-threshold 800000))
