9633bc050aa42c0ae7c5504f078d6e3046a78e9e
Endi Merkuri <merkuriendi@gmail.com>
2025-08-05 14:32:18 +0200

    nvim: show diagnostics only with color in number column

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -11,17 +11,16 @@ vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.wrap = false
 vim.o.scrolloff = 8
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = "yes:1"
 vim.o.termguicolors = true
 vim.o.cursorline = true
 vim.o.hidden = true
--- vim.o.cmdheight = 0
 vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.laststatus = 3
-vim.g.gruvbox_material_background = "hard"
+-- vim.g.gruvbox_material_background = "hard"
 
 local function augroup(name)
 	return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
@@ -42,4 +41,21 @@ vim.filetype.add({
 })
 
 vim.lsp.enable({ "csharp_ls", "gopls", "intelephense", "pylsp", "ts_ls", "rust_analyzer" })
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+	virtual_text = true,
+	-- Use only color to show diagnostics in sign column
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+		},
+	},
+})
