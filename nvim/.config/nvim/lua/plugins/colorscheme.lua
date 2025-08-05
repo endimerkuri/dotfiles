182aca5d1fa3e61509c500d0102b0dfc6657b670
Endi Merkuri <merkuriendi@gmail.com>
2025-08-05 09:59:33 +0200

    nvim: remove mini.nvim, change colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
-		name = "gruvbox-material",
+		"vague2k/vague.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme gruvbox-material]])
+			vim.cmd([[colorscheme vague]])
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
 		end,
 	},
 }
